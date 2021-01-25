245d6139eac8e138fc5b3a6b5dd93e01212ac550
“DavidOnofre24” <davidonofre1010@gmail.com>
2021-01-25 17:33:06 -0600

    Inicio tab2

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/pages/tab1_page.dart';
+import 'package:news_app/src/pages/tab2_page.dart';
 import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
@@ -44,9 +45,7 @@ class _Paginas extends StatelessWidget {
       physics: NeverScrollableScrollPhysics(),
       children: [
         Tab1Page(),
-        Container(
-          color: Colors.green,
-        )
+        Tab2Page(),
       ],
     );
   }
