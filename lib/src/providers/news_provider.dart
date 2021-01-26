609155ace487e5dc90dcdde7449a61e056fce982
“DavidOnofre24” <davidonofre1010@gmail.com>
2021-01-26 13:17:41 -0600

    Lista de categorias

--- lib/src/providers/news_provider.dart
+++ lib/src/providers/news_provider.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:news_app/src/models/category_model.dart';
 import 'package:news_app/src/models/news_models.dart';
 import 'package:http/http.dart' as http;
 
 class NewsProvider with ChangeNotifier {
   List<Article> encabezados = [];
+
+  List<Category> categorias = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.footballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology')
+  ];
+
   final _urlNews = 'https://newsapi.org/v2';
   final _apiKey = '937f21122c2e4e82a95ef75290304c21';
 
