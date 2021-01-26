05bf7a0febd56dbb219b69e2231231396ef9d09b
“DavidOnofre24” <davidonofre1010@gmail.com>
2021-01-26 13:07:44 -0600

    modelo de categoria

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -3,8 +3,15 @@ import 'package:news_app/src/providers/news_provider.dart';
 import 'package:news_app/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
   @override
+  _Tab1PageState createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  // ignore: must_call_super
   Widget build(BuildContext context) {
     final newsProvider = Provider.of<NewsProvider>(context);
     return Scaffold(
@@ -14,4 +21,8 @@ class Tab1Page extends StatelessWidget {
               )
             : ListaNoticias(newsProvider.encabezados));
   }
+
+  @override
+  bool get wantKeepAlive =>
+      true; // con esto, la pagina no vuelve al inicio(Se queda en donde el usuario habia quedado)
 }
